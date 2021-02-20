67dcc93f87bd0140bc95818cf717eb69298d80b7
egorble <70462219+egorble@users.noreply.github.com>
2021-02-20 11:37:13 +0200

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["internetshopdiplom.herokuapp.com/]
+ALLOWED_HOSTS = ["internetshopdiplom.herokuapp.com"]
 
 
 # Application definition
