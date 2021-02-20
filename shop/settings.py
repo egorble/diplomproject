4e14b74c64eac3a914b910684f8c87e49603e477
egorble <70462219+egorble@users.noreply.github.com>
2021-02-20 11:26:41 +0200

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -137,5 +137,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static_dev'),
 )
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
